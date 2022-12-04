2cda5a5322f91406dd2b1a3d3c655afd93931670
Hong Xiang <hx.hongxiang@gmail.com>
2022-12-04 22:26:25 +0800

    🚧 work in progress

--- ParallelPI/CoreCLR/ParallelPI/Program.cs
+++ ParallelPI/CoreCLR/ParallelPI/Program.cs
@@ -10,14 +10,33 @@ using System.Reactive.Linq;
 using System.Reactive;
 using System.Reactive.Concurrency;
 
+struct PiCalcuateChunk
+{
+    double Calculate(int start, int end, int step)
+    {
+        var result = 0.0;
+        for (int i = start; i < end; i++)
+        {
+            var x = (i + 0.5) * step;
+            var f = 4.0 / (1.0 + x * x);
+            result += f * step;
+        }
+        return result;
+    }
+}
+
+interface ParallelPICalculator
+{
+    double Calculate(int chunks, int stepsPreChunk);
+}
+
 namespace ParallelPI
 {
-
-
     internal class Program
     {
-        const int NumberOfSteps = 1_000_000_000;
         const int Chunks = 65536;
+        const int ChunkSize = 65536;
+        const int NumberOfSteps = 1_000_000_000;
         //const int Chunks = 30000;
 
         /// <summary>Main method to time various implementations of computing PI.</summary>
@@ -30,12 +49,13 @@ namespace ParallelPI
             //Time(ParallelLinqPi, nameof(ParallelLinqPi));
             //Time(SerialPi, nameof(SerialPi));
             //Time(ParallelPi, nameof(ParallelPi));
-            for (int i = 0; i < 32; i++)
+            foreach (var _ in Enumerable.Range(0, 12))
             {
                 Time(ParallelPartitionerPi, nameof(ParallelPartitionerPi));
-                //Time(ParallelTaskPi, nameof(ParallelTaskPi));
-                //Time(ParallelRxPi, nameof(ParallelRxPi));
             }
+            Time(ParallelTaskPi, nameof(ParallelTaskPi));
+            Time(ParallelRxPi, nameof(ParallelRxPi));
+            Time(ParallelAsyncTaskPi, nameof(ParallelAsyncTaskPi));
         }
 
         /// <summary>Times the execution of a function and outputs both the elapsed time and the function's result.</summary>
@@ -123,26 +143,64 @@ namespace ParallelPI
             double step = 1.0 / (double)NumberOfSteps;
             var chunkSize = (int)Math.Ceiling((float)NumberOfSteps / Chunks);
             Console.WriteLine($"chunks {Chunks}, chunk size {chunkSize}");
-            var tasks = new Task<double>[Chunks];
-            var starts = new int[Chunks];
-            var ends = new int[Chunks];
-            for (int i = 0; i < Chunks; i++)
+            var tasks = Enumerable.Range(0, Chunks).Select(index =>
             {
-                tasks[i] = Task.Factory.StartNew((indexObj) =>
+                return Task.Factory.StartNew(() =>
+                                {
+                                    var start = Math.Min(index * chunkSize, NumberOfSteps - 1);
+                                    var end = Math.Min((index + 1) * chunkSize, NumberOfSteps);
+                                    double local = 0.0;
+                                    for (int i = start; i < end; i++)
+                                    {
+                                        double x = (i + 0.5) * step;
+                                        local += 4.0 / (1.0 + x * x);
+                                    }
+                                    return local;
+                                });
+            }).ToArray();
+            return Task.WhenAll(tasks).Result.Sum() / NumberOfSteps;
+        }
+
+        static async Task<double> SumOfPiSamples(int index, int chunkSize, double step)
+        {
+            return await Task.Factory.StartNew(() =>
+            {
+                var start = Math.Min(index * chunkSize, NumberOfSteps - 1);
+                var end = Math.Min((index + 1) * chunkSize, NumberOfSteps);
+                double local = 0.0;
+                for (int i = start; i < end; i++)
                 {
-                    var index = (indexObj as IndexValue).Value;
-                    var start = Math.Min(index * chunkSize, NumberOfSteps - 1);
-                    var end = Math.Min((index + 1) * chunkSize, NumberOfSteps);
-                    double local = 0.0;
-                    for (int i = start; i < end; i++)
-                    {
-                        double x = (i + 0.5) * step;
-                        local += 4.0 / (1.0 + x * x);
-                    }
-                    return local;
-                }, new IndexValue { Value = i });
+                    double x = (i + 0.5) * step;
+                    local += 4.0 / (1.0 + x * x);
+                }
+                return local;
+            });
+        }
+
+        static async ValueTask<double> SumOfPiMultipleSamples(int chunkStart, int chunkEnd, int chunkSize, double step)
+        {
+            double local = 0.0;
+            for (int ic = chunkStart; ic < chunkEnd; ic++)
+            {
+                local += await SumOfPiSamples(ic, chunkSize, step);
             }
-            return Task.WhenAll(tasks).Result.Sum();
+            return local;
+        }
+
+        static double ParallelAsyncTaskPi()
+        {
+            double step = 1.0 / (double)NumberOfSteps;
+            var chunkSize = (int)Math.Ceiling((float)NumberOfSteps / Chunks);
+            Console.WriteLine($"chunks {Chunks}, chunk size {chunkSize}");
+            var groups = 128;
+            var groupSize = (int)Math.Ceiling((float)Chunks / groups);
+            var tasks = Enumerable.Range(0, groupSize).Select(async (ig) =>
+                        {
+                            var chunkStart = Math.Min(ig * groupSize, Chunks - 1);
+                            var chunkEnd = Math.Min((ig + 1) * groupSize, Chunks);
+                            return await SumOfPiMultipleSamples(chunkStart, chunkEnd, chunkSize, step);
+                        });
+            return Task.WhenAll(tasks).Result.Sum() / NumberOfSteps;
         }
 
         static double ParallelRxPi()
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # CoreCLRUnityBenchmark
+
+## ParallelPI
+
+Inspired by [.NET Core parallel computation of PI example by Microsoft](https://learn.microsoft.com/en-us/samples/dotnet/samples/parallel-programming-compute-pi-cs/)
+
+Calculating integral of 4/(1 + x^2) over 0 to 1, which should return PI. Integral is approximated by weighted sum of sample points i / N, i = 0 .. N - 1.
+
+
+
